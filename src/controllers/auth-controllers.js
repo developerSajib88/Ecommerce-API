6e630493f3ddd3c0f111ab8b8788fa1cd3e3e866
developerSajib88 <developersajib88@gmail.com>
2025-03-23 00:31:42 +0600

    Some controller created for auth

--- src/controllers/auth-controllers.js
+++ src/controllers/auth-controllers.js
@@ -1,2 +1,5 @@
 module.exports.login = (req,res)=>{}
+module.exports.logout = (req,res)=>{}
 module.exports.register = (req,res)=>{}
+module.exports.forgetPassword = (req,res)=>{}
+module.exports.resetPassword = (req,res)=>{}
